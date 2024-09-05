98a1a27dbf508901d6a9f7f3e8c3e2258f8df7c3
Daniil Solovei <gotodeeps@gmail.com>
2024-09-05 21:03:26 +0700

    feat: add 2 mode of work

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,36 @@
+// main.go
 package main
 
 import (
-	"fmt"
+	_ "fmt"
+	"log/slog"
 	"os"
 
+	"dirTree/internal/config"
+	"dirTree/internal/scanner"
 	"dirTree/internal/tui"
 )
 
 func main() {
-	if err := tui.Run(); err != nil {
-		fmt.Printf("Error: %v\n", err)
+	cfg := config.ParseFlags()
+
+	if cfg.TUIMode {
+		// Передаём cfg в RunTUI
+		selectedDirs, err := tui.RunTUI(cfg)
+		if err != nil {
+			slog.Error("Ошибка в интерактивном режиме", slog.String("error", err.Error()))
+			os.Exit(1)
+		}
+		cfg.SelectedDirs = selectedDirs
+	}
+
+	if cfg.RelativeFlag && cfg.AbsoluteFlag {
+		slog.Error("Взаимоисключающие флаги: --relative и --absolute не могут быть установлены одновременно.")
+		os.Exit(1)
+	}
+
+	if err := scanner.Run(cfg); err != nil {
+		slog.Error("Ошибка", slog.String("error", err.Error()))
 		os.Exit(1)
 	}
 }
