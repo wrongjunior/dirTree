fdcd34407dac79ba1398c48bf5923ea11c991bfc
Daniil Solovei <gotodeeps@gmail.com>
2024-09-05 11:29:27 +0700

    feat: Add tui

--- cmd/main.go
+++ cmd/main.go
@@ -1,23 +1,15 @@
 package main
 
 import (
-	"log/slog"
+	"fmt"
 	"os"
 
-	"dirTree/internal/config"
-	"dirTree/internal/scanner"
+	"dirTree/internal/tui"
 )
 
 func main() {
-	cfg := config.ParseFlags()
-
-	if cfg.RelativeFlag && cfg.AbsoluteFlag {
-		slog.Error("Взаимоисключающие флаги: --relative и --absolute не могут быть установлены одновременно.")
-		os.Exit(1)
-	}
-
-	if err := scanner.Run(cfg); err != nil {
-		slog.Error("Ошибка", slog.String("error", err.Error()))
+	if err := tui.Run(); err != nil {
+		fmt.Printf("Error: %v\n", err)
 		os.Exit(1)
 	}
 }
